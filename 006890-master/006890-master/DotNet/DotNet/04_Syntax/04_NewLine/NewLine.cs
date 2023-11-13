26637b5f41c401d7a9caec181bc70570d7095088
박상준 <@DESKTOP-D0PRH9L>
2023-11-13 17:09:04 +0900

    231113

---
+++ 006890-master/006890-master/DotNet/DotNet/04_Syntax/04_NewLine/NewLine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class NewLine
+{
+    static void Main()
+    {
+        Console.WriteLine("줄\n바꿈");
+        
+    }
+}
---
+++ 006890-master/006890-master/DotNet/DotNet/04_Syntax/EmptyStatement/EmptyStatement.cs
@@ -0,0 +1,10 @@
+﻿// 공문
+using System;
+
+class EmptyStatement
+{
+    static void Main()
+    {
+        Console.WriteLine("hello");
+    }
+}
