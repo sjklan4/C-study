2932a6cbb2009fd38b3212b6e8e0781f7a8a11ef
sjklan4 <dpeltms4@gmail.com>
2023-11-14 23:09:21 +0900

    231114

---
+++ 006890-master/006890-master/Dul/Debugtest/Debugtest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class DebugDemo
+{ 
+    static void Main()
+    {
+        // int number1 = 10;
+        // int number2 = 20;
+        // int number3 = 30;
+
+        // Console.WriteLine(number1);
+        // Console.WriteLine(number2);
+        // Console.WriteLine(number3); 
+        int a, b, c, r;
+        a = 3;
+        b = 5;
+        c = 7;
+
+        r = a-- - ++b * c++;
+        Console.WriteLine("r : {0}, a : {1}, b : {2}, c :{3}", r,a,b,c);
+    }
+}
