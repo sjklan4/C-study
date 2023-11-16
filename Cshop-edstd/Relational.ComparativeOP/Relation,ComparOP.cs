92b4febdfec5dedab3211607be35ab4d9a7db359
sjklan4 <dpeltms4@gmail.com>
2023-11-16 09:41:16 +0900

    231116

---
+++ Cshop-edstd/Relational.ComparativeOP/Relation,ComparOP.cs
@@ -0,0 +1,18 @@
+﻿//비교 연산자
+// >,<,>=,==(같은지), !=
+using System;
+
+public class relationOP
+{
+    public static void Main()
+    {
+        int firstNum = 10;
+        int secondNum = 5;
+        bool r = false;
+
+        Console.WriteLine(firstNum > secondNum); 
+
+        r = (firstNum <= secondNum); // false
+        Console.WriteLine(r);
+    }
+}
