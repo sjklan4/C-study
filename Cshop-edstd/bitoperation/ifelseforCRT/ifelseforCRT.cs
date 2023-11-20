6673453ddb49aaa1365cbdd35ed1b8246b1f7931
sjklan4 <dpeltms4@gmail.com>
2023-11-20 13:06:42 +0900

    231120if구문

---
+++ Cshop-edstd/bitoperation/ifelseforCRT/ifelseforCRT.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class If
+{
+    static void Main()
+    {
+        int score = 90;
+        if (score >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if(score >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+    }
+
+}
