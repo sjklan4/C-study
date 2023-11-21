53b7f2199a4f47c88f854fa73d3c2538ca3de999
sjklan4 <dpeltms4@gmail.com>
2023-11-21 18:54:02 +0900

    231121

---
+++ Cshop-edstd/bitoperation/ForContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿// for(초기식; 조건식; 증감식){}
+
+// 짝수 구하기 
+
+using System;
+
+class ForSum
+{
+    static void Main()
+    {
+        int n = 5;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) 
+        {
+            if (i % 2 == 0)
+            {
+                sum += i; // 짝수 0,2,4 더하기
+            }
+
+        }
+        Console.WriteLine($" 1부터 {n}까지 짝수의 합 : {sum}");
+    }
+}
---
+++ Cshop-edstd/bitoperation/whileOp/Program.cs
@@ -0,0 +1,52 @@
+﻿//while(무조건 반복문)
+
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class whileop{
+    static void Main()
+    {
+        int sum = 0;
+        int i = 1;
+
+        //while (sum <= 3)
+        //{
+        //    Console.WriteLine("테스트");
+        //}
+
+        //do
+        //{
+        //    sum += i;
+        //    i++;
+        //} while (i <= 5);
+        //Console.WriteLine($"합계 : {sum}");
+
+        //do
+        //{
+        //    if (i % 3 == 0 && i % 4 == 0)
+        //    {
+        //        sum += i;
+        //    }
+        //    i++;
+        //} while (i <= 100);
+        //Console.WriteLine(sum);
+
+
+        //string[] names = { "C#", "ASP.NET" };
+
+        //foreach (string name in names)
+        //{
+        //    Console.WriteLine(name);
+        //}
+
+        //string str = "ABC123";
+        //foreach (char c in str)
+        //{
+        //    Console.WriteLine(c);
+        //}
+
+    }
+
+
+}
